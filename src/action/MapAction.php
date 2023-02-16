4768a1960b080776b0caea04d0d7574c5a18ec11
Paul <paul5mass5@gmail.com>
2023-02-16 13:12:11 +0100

    adding products map with geolocalization and popups

--- src/action/MapAction.php
+++ src/action/MapAction.php
@@ -9,25 +9,35 @@ class MapAction
 
     public function execute(): string
     {
-
-        $catalogue  = '<div class="container mt-3">';
-        
-        // script for map creation using leaflet
-        $catalogue .= '<script>';
-        $catalogue .= 'var mymap = L.map("mapid").setView([48.856614, 2.3522219], 13);';
-        $catalogue .= 'var osm = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {';
-        $catalogue .= 'attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",';
-        $catalogue .= 'maxZoom: 18,';
-        $catalogue .= 'id: "mapbox/streets-v11",';
-        $catalogue .= 'tileSize: 512,';
-        $catalogue .= 'zoomOffset: -1});';
-        $catalogue .= 'osm.addTo(mymap);';
-        $catalogue .= '</script>';
-
-        $catalogue .= '</div>';
+        $catalogue  = '<div class="container"><div id="map"></div></div>';
 
         // add leaflet script
         $catalogue .= '<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>';
+
+        // script for map creation using leaflet
+        $catalogue .= '<script>';
+
+        $catalogue .= <<<END
+            document.getElementById("map").style.height = window.innerHeight + "px";
+            var map = L.map("map").setView([48.856614, 2.3522219], 13);
+            navigator.geolocation.getCurrentPosition((position) => {
+                map.setView([position.coords.latitude, position.coords.longitude], 13);
+            });
+            var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            });
+            osm.addTo(map);
+        END;
+
+        $produits = Produit::All();
+        foreach ($produits as $produit) {
+            $catalogue .= <<<END
+                var marker = L.marker([$produit->latitude, $produit->longitude]).addTo(map);
+                marker.bindPopup(`<b>$produit->nom</b><br>$produit->lieu<br><a href='index.php?action=productDetails&id=$produit->id'>Voir le produit</a>`);
+            END;
+        }
+
+        $catalogue .= '</script>';
 
         return $catalogue;
     }
