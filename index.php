e67062591ccaf05c4432b4cf43c734d56ac67f6d
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 11:00:52 +0100

    :bug: Correction index.php

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
 <?php
+declare(strict_types=1);
+require 'vendor/autoload.php';
+session_start();
 
-require 'header.php';
+use crazy\dispatch\Dispatcher;
 
-echo '<div class="container my-3">
-<div class="row align-items-center">';
-
-use crazy\models\Produit;
-$products = Produit::all();
-foreach ($products as $product) {
-    echo <<<END
-        <div class="col">
-            <div class="card" style="width: 18rem">
-                <img class="card-img-top" src="src/img/{$product->id}.jpg" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">{$product->nom}</h5>
-                    <p class="card-text">$product->description</p>
-                    <a href="#" class="btn btn-primary">Ajouter au panier</a>
-                </div>
-            </div>
-        </div>
-    END;
-}
-
-echo '</div></div>';
-
-require 'footer.php';
+$_GET['action'] = $_GET['action'] ?? "";
+$dispatcher = new Dispatcher($_GET['action']);
+$dispatcher->run();
