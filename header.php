b6449174836a246a7f878edf125289d75505b044
Dami1 <damien.melchior3@etu.univ-lorraine.fr>
2023-02-16 11:20:46 +0100

    Modif header.php

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ $db->bootEloquent();
 <body>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light py-0">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="src/img/logo.png" alt="Court Circuit Nancy" style="width: 3rem"></a>
+            <a class="navbar-brand" href="index.php"><img src="src/img/logo.png" alt="Court Circuit Nancy" style="width: 3rem"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
