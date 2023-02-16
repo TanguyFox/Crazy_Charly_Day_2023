d47b937d522587cb2c3fe8165d27913c847f5893
BJulOs <84396590+BJulOs@users.noreply.github.com>
2023-02-16 10:15:23 +0100

    mise en place architecture mvc

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 require 'header.php';
 
-use netvod\dispatch\Dispatcher;
+use crazy\dispatch\Dispatcher;
 
 session_start();
 $_GET['action'] = $_GET['action'] ?? "";
---
+++ src/models/Produit.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace crazy\models;
+
+class Produit {
+
+}
