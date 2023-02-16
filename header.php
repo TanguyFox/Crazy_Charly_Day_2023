437810fede9f994d9d36d4bf8010e8e801c2b9b1
Dami1 <damien.melchior3@etu.univ-lorraine.fr>
2023-02-16 11:08:25 +0100

    Ajout du logo

--- header.php
+++ header.php
@@ -27,6 +27,7 @@ $db->bootEloquent();
 <body>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
+            <img src="src/img/logo.png" alt="Court Circuit Nancy" style="width: 3rem; margin-right: 1rem">
             <a class="navbar-brand" href="#">CourtCircuit Voltaire</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
