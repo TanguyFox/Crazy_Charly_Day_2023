4af307e264a0554dcb9d8f10d19c40bc2f6659bf
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 13:49:22 +0100

    :construction: en construction

--- src/action/AddToCartAction.php
+++ src/action/AddToCartAction.php
@@ -4,13 +4,16 @@ namespace crazy\action;
 
 use crazy\models\Produit;
 
-class AddToChartAction
+class AddToCartAction
 {
     public function execute(): string
     {
-        $id = $_GET['id'] ?? "";
-        $product = Produit::where('id', $id)->first();
-        $html = <<<HTML
+        if (!isset($_SESSION['user'])) {
+            header('Location: index.php?action=login&redirect=addToCart&error=needConnection');
+        } else {
+            $id = $_GET['id'] ?? "";
+            $product = Produit::where('id', $id)->first();
+            $html = <<<HTML
 <section class="h-100 h-custom" style="background-color: #eee;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
@@ -32,33 +35,7 @@ class AddToChartAction
                   </div>
                 </div>
 HTML;
-
-                <div class="card mb-3">
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <div class="d-flex flex-row align-items-center">
-                        <div>
-                          <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
-                        </div>
-                        <div class="ms-3">
-                          <h5>Iphone 11 pro</h5>
-                          <p class="small mb-0">256GB, Navy Blue</p>
-                        </div>
-                      </div>
-                      <div class="d-flex flex-row align-items-center">
-                        <div style="width: 50px;">
-                          <h5 class="fw-normal mb-0">2</h5>
-                        </div>
-                        <div style="width: 80px;">
-                          <h5 class="mb-0">$900</h5>
-                        </div>
-                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+            if ()
 
                 <div class="card mb-3">
                   <div class="card-body">
