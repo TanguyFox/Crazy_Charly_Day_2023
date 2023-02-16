eeb12ae5ff4523c4451b423d3000c6eeb2ea11b4
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 14:56:49 +0100

    :sparkles: Ajout de l'affichage du panier

---
+++ src/action/DisplayCartAction.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace crazy\action;
+
+use crazy\models\Cart;
+use crazy\models\Produit;
+
+class DisplayCartAction
+{
+    public function execute(): string
+    {
+        $html = "";
+        if (!isset($_SESSION['user']))
+            header('Location: index.php?action=login&error=needConnection');
+        else {
+            $prixTotal = 0;
+            $idUser = $_SESSION['user']->id;
+            $cartProducts = Cart::where('user_id', $idUser)->get();
+            $html = <<<HTML
+<section class="h-100 h-custom" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col">
+        <div class="card">
+          <div class="card-body p-4">
+
+            <div class="row">
+
+              <div class="col-lg-7">
+                <h5 class="mb-3"><a href="#!" class="text-body"><i
+                      class="fas fa-long-arrow-alt-left me-2"></i>Retourner à la boutique</a></h5>
+                <hr>
+
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                  <div>
+                    <p class="mb-1">Panier</p>
+                    <p class="mb-0">Vous avez ? produit dans votre panier</p>
+                  </div>
+                </div>
+HTML;
+
+            if ($cartProducts->count() == 0) {
+                $html .= "<h3>Votre panier est vide</h3>";
+            } else {
+                foreach ($cartProducts as $cP) {
+                    $product = Produit::where('id', $cP->product_id)->first();
+                    $prixTotal += $product->prix * $cP->quantite;
+                    $html .= <<<HTML
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex flex-row align-items-center">
+                        <div>
+                          <img
+                            src="src/img/{$product->id}.jpg"
+                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                        </div>
+                        <div class="ms-3">
+                          <h5>{$product->nom}</h5>
+                          <p class="small mb-0">{$product->lieu}</p>
+                        </div>
+                      </div>
+                      <div class="d-flex flex-row align-items-center">
+                        <div style="width: 50px;">
+                          <h5 class="fw-normal mb-0">{$cP->quantite}</h5>
+                        </div>
+                        <div style="width: 80px;">
+                          <h5 class="mb-0">{$product->prix}</h5>
+                        </div>
+                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+HTML;
+                }
+            }
+
+            $html .= <<<HTML
+            <div class="col-lg-5" >
+
+                <div class="card bg-primary text-white rounded-3" >
+                  <div class="card-body" >
+                    <div class="d-flex justify-content-between align-items-center mb-4" >
+                      <h5 class="mb-0" >Détails de la carte</h5 >
+                      <img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                        class="img-fluid rounded-3" style = "width: 45px;" alt = "Avatar" >
+                    </div >
+
+                    <p class="small mb-2" >Type de carte</p >
+                    <a href = "#!" type = "submit" class="text-white" ><i
+                        class="fab fa-cc-mastercard fa-2x me-2" ></i ></a >
+                    <a href = "#!" type = "submit" class="text-white" ><i
+                        class="fab fa-cc-visa fa-2x me-2" ></i ></a >
+                    <a href = "#!" type = "submit" class="text-white" ><i
+                        class="fab fa-cc-amex fa-2x me-2" ></i ></a >
+                    <a href = "#!" type = "submit" class="text-white" ><i class="fab fa-cc-paypal fa-2x" ></i ></a >
+
+                    <form class="mt-4" >
+                      <div class="form-outline form-white mb-4" >
+                        <input type = "text" id = "typeName" class="form-control form-control-lg" siez = "17"
+                          placeholder = "Cardholder's Name" />
+                        <label class="form-label" for="typeName" >Nom du titulaire</label>
+                      </div>
+
+                      <div class="form-outline form-white mb-4">
+                        <input type="text" id="typeText" class="form-control form-control-lg" siez="17"
+                          placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
+                        <label class="form-label" for="typeText">Numéro de carte</label>
+                      </div>
+
+                      <div class="row mb-4">
+                        <div class="col-md-6">
+                          <div class="form-outline form-white">
+                            <input type="text" id="typeExp" class="form-control form-control-lg"
+                              placeholder="MM/YYYY" size="7" id="exp" minlength="7" maxlength="7" />
+                            <label class="form-label" for="typeExp">Expiration</label>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-outline form-white">
+                            <input type="password" id="typeText" class="form-control form-control-lg"
+                              placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" />
+                            <label class="form-label" for="typeText">CVV</label>
+                          </div>
+                        </div>
+                      </div>
+
+                    </form>
+
+                    <hr class="my-4">
+                    <div class="d-flex justify-content-between mb-4">
+                      <p class="mb-2">Total(Incl. taxes)</p>
+                      <p class="mb-2">{$prixTotal}</p>
+                    </div>
+
+                    <button type="button" class="btn btn-info btn-block btn-lg">
+                      <div class="d-flex justify-content-between">
+                        <span>{$prixTotal}</span>
+                        <span>Payer<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+                      </div>
+                    </button>
+
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+HTML;
+        }
+        return $html;
+    }
+}
