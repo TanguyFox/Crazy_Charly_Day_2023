4d01f4627000c3c09ef91d43f0abaf9cdb7414f4
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 15:38:33 +0100

    :hammer: modification de l'ajout de produit

--- src/action/AddToCartAction.php
+++ src/action/AddToCartAction.php
@@ -12,18 +12,24 @@ class AddToCartAction
         //redirection vers la page d'origine
         $redirection = $_GET['redirection'];
         //si l'utilisateur n'est pas connecté, on le redirige vers la page de connexion
-        if (!isset($_SESSION['user'])) {
+        /*if (!isset($_SESSION['user'])) {
             header('Location: index.php?action=login&redirection=' . $redirection . '&error=needConnection');
         } else {
             //sinon on ajoute le produit au panier et on le redirige vers la page d'origine
             $idProduct = $_GET['id'] ?? "";
             $idUser = $_SESSION['user']->id;
-            $cart = new Cart();
-            $cart->product_id = $idProduct;
-            $cart->user_id = $idUser;
-            $cart->save();
+            if (Cart::where('user_id', $idUser)->where('product_id', $idProduct)->exists()) {
+                $cart = Cart::where('user_id', $idUser)->where('product_id', $idProduct)->first();
+                $cart->quantite += 1;
+                $cart->save();
+            } else {
+                $cart = new Cart();
+                $cart->product_id = $idProduct;
+                $cart->user_id = $idUser;
+                $cart->save();
+            }*/
             header('Location: index.php?action=' . $redirection);
-        }
+        //}
         return "";
     }
 }
