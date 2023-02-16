ed4e40d35d02265c1e105323278336f8bf2ad6e7
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 14:59:07 +0100

    :fire: Suppression de lien entre classe inutile

--- src/models/Users.php
+++ src/models/Users.php
@@ -9,10 +9,4 @@ class Users extends Eloquent\Model {
 	protected $table = 'users';
 	protected $primaryKey = 'id';
 	public $timestamps = false;
-
-    public function panier(): Eloquent\Relations\BelongsToMany
-    {
-        return $this->belongsToMany(Produit::class, 'cart', 'id', 'id');
-    }
-
 }
