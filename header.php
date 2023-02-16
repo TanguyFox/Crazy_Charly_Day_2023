a2ba8e06235d6e828d8690ad04c3f9c4f839870e
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-02-16 10:46:06 +0100

    :hammer: Déplacement de la page dans le Dispatcher.php

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ $db->bootEloquent();
 <body>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">CourtCircuit Voltaire</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php
 
-require 'header.php';
+require 'vendor/autoload.php';
 
-echo '<div class="container mt-3">
-<div class="row">';
+use crazy\dispatch\Dispatcher;
 
-use crazy\models\Produit;
-$products = Produit::all();
-foreach ($products as $product) {
-    echo <<<END
-        <div class="col">
-            <div class="card" style="width: 18rem">
-                <img class="card-img-top" src="src/img/{$product->id}.jpg" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">{$product->nom}</h5>
-                    <p class="card-text">$product->description</p>
-                    <a href="#" class="btn btn-primary">Ajouter au panier</a>
-                </div>
-            </div>
-        </div>
-    END;
-}
-
-echo '</div></div>';
-
-require 'footer.php';
+$_GET['action'] = $_GET['action'] ?? "";
+$dispatcher = new Dispatcher($_GET['action']);
+$dispatcher->run();
