af2018cf862b9e6e8de5f2a82333c65e56bdce40
BJulOs <84396590+BJulOs@users.noreply.github.com>
2023-02-16 10:18:14 +0100

    Correction Categorie + Produit

--- src/models/Categorie.php
+++ src/models/Categorie.php
@@ -2,15 +2,14 @@
 
 namespace crazy\models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent as Eloquent;
 
-class Categorie extends Model
-{
+class Categorie extends Eloquent\Model {
     protected $table = 'produit';
     protected $primaryKey = 'id';
     public $timestamps = false;
 
-    public function produits() {
+    public function produits() : Eloquent\Relations\HasMany {
         return $this->hasMany('models\Produit', 'categorie','id');
     }
 }
--- src/models/Produit.php
+++ src/models/Produit.php
@@ -2,15 +2,14 @@
 
 namespace crazy\models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent as Eloquent;
 
-class Produit extends Model
-{
+class Produit extends Eloquent\Model {
     protected $table = 'produit';
     protected $primaryKey = 'id';
     public $timestamps = false;
 
-    public function categorie() {
+    public function categorie() : Eloquent\Relations\BelongsTo {
         return $this->belongsTo('models\categorie', 'categorie');
     }
 }
